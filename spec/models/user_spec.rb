67761d5009e9fa45240dc0f9d61893593a25fb6d
wendy li <wenhua.li@gmail.com>
2012-08-16 23:42:51 -0700

    Make a basic User model (including secure passwords)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com", 
-                     password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
+
 
   subject { @user }
 
@@ -15,25 +15,8 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
-  #it { should be_valid }
+  it { should be_valid }
   
-
-
- # describe "when name is not present" do
- #   before { @user.name = " " }
- #   it { should_not be_valid }
- # end
-
- 
- # describe "when email address is already taken" do
- #    before do
- #      user_with_same_email = @user.dup
- #      user_with_same_email.save
- #    end
-
- #    it { should_not be_valid }
- #  end
-
 
 
 end
